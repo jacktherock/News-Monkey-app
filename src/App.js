8312d68104c2dc4bd9b24f1a84bd0c8ffcfb9647
Abhijeet <sonawaneabhijeet273@gmail.com>
2021-08-27 13:04:31 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// class based component
+export default class App extends Component {
+
+  // a = "Abhijeet"
+
+  render() {
+    return (
+      <div>
+        {/* This is {this.a} */}
+
+        <Navbar />
+        <News category="general" heading="Top Headlines" country="in" pageSize={6}/>
+
+      </div>
+    )
+  }
 }
-
-export default App;
