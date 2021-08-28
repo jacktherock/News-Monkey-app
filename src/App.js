6474c3ba4b2a6fac15dc53b62b8f2b76d99f0471
Abhijeet <sonawaneabhijeet273@gmail.com>
2021-08-28 22:00:30 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
+import LoadingBar from 'react-top-loading-bar';
 import News from './components/News';
 import {
   BrowserRouter as Router,
@@ -13,22 +14,40 @@ export default class App extends Component {
 
   // a = "Abhijeet"
   pageSize = 6;
+
+  state = {
+    progress: 0
+  }
+
+  setProgress = (progress) => {
+    this.setState({progress: progress})
+  }
+
   render() {
     return (
       <div>
         {/* This is {this.a} */}
         <Router>
+
           <Navbar />
           {/* <News category="general" heading="Top Headlines" country="in" pageSize={this.pageSize} /> */}
 
+          {/* Top loader */}
+          <LoadingBar
+            height= {3}
+            color='#f11946'
+            progress={this.state.progress}
+          />
+
+          {/* Categories */}
           <Switch>
-            <Route exact path="/"><News key="general" category="general" country="in" pageSize={this.pageSize} /></Route>
-            <Route exact path="/business"><News key="business" category="business" country="in" pageSize={this.pageSize} /></Route>
-            <Route exact path="/entertainment"><News key="entertainment" category="entertainment" country="in" pageSize={this.pageSize} /></Route>
-            <Route exact path="/health"><News key="health" category="health" country="in" pageSize={this.pageSize} /></Route>
-            <Route exact path="/science"><News key="science" category="science" country="in" pageSize={this.pageSize} /></Route>
-            <Route exact path="/sports"><News key="sports" category="sports" country="in" pageSize={this.pageSize} /></Route>
-            <Route exact path="/technology"><News key="technology" category="technology" country="in" pageSize={this.pageSize} /></Route>
+            <Route exact path="/"><News setProgress={this.setProgress} key="general" category="general" country="in" pageSize={this.pageSize} /></Route>
+            <Route exact path="/business"><News setProgress={this.setProgress} key="business" category="business" country="in" pageSize={this.pageSize} /></Route>
+            <Route exact path="/entertainment"><News setProgress={this.setProgress} key="entertainment" category="entertainment" country="in" pageSize={this.pageSize} /></Route>
+            <Route exact path="/health"><News setProgress={this.setProgress} key="health" category="health" country="in" pageSize={this.pageSize} /></Route>
+            <Route exact path="/science"><News setProgress={this.setProgress} key="science" category="science" country="in" pageSize={this.pageSize} /></Route>
+            <Route exact path="/sports"><News setProgress={this.setProgress} key="sports" category="sports" country="in" pageSize={this.pageSize} /></Route>
+            <Route exact path="/technology"><News setProgress={this.setProgress} key="technology" category="technology" country="in" pageSize={this.pageSize} /></Route>
           </Switch>
         </Router>
       </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,21 +6,27 @@ export class NewsItem extends Component {
 
     render() {
         let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
-        
+
         return (
             <div className="my-3">
                 <div className="card">
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        position:'absolute',
+                        right: 0
+                    }}>
+                        <span className="badge rounded-pill bg-danger">
+                            {source}
+                        </span>
+                    </div>
+
                     <img src={!imageUrl ? "https://c.ndtvimg.com/2021-08/uflv8vc_tech_640x480_27_August_21.jpg" : imageUrl} className="card-img-top" alt="..." />
+
                     <div className="card-body">
 
                         <h5 className="card-title">{title}...</h5>
-
-                        <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex:'1'}}>
-                            {source}
-                        </span>
-
                         <p className="card-text">{description}...</p>
-
                         <p className="card-text"><small className="text-muted">By <strong>{!author ? "Unknown" : author}</strong> on <strong>{new Date(date).toGMTString()}</strong></small></p>
 
                         <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read more...</a>
