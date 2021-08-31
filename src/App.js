90de854c9d49ca55f82d74224106f967eccc0763
Abhijeet <sonawaneabhijeet273@gmail.com>
2021-08-31 21:41:52 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar';
 import LoadingBar from 'react-top-loading-bar';
 import News from './components/News';
@@ -10,48 +10,42 @@ import {
 } from "react-router-dom";
 
 // class based component
-export default class App extends Component {
+const App = () => {
 
   // a = "Abhijeet"
-  pageSize = 6;
-  apiKey = process.env.REACT_APP_NEWS_API
+  const pageSize = 6;
+  const apiKey = process.env.REACT_APP_NEWS_API
 
-  state = {
-    progress: 0
-  }
+  const [progress, setProgress] = useState(0)
 
-  setProgress = (progress) => {
-    this.setState({ progress: progress })
-  }
+  return (
+    <div>
+      {/* This is {a} */}
 
-  render() {
-    return (
-      <div>
-        {/* This is {this.a} */}
+      <Router>
+        <Navbar />
+        {/* <News category="general" heading="Top Headlines" country="in" pageSize={pageSize} /> */}
 
-        <Router>
-          <Navbar />
-          {/* <News category="general" heading="Top Headlines" country="in" pageSize={this.pageSize} /> */}
+        {/* Top loader */}
+        <LoadingBar
+          height={3}
+          color='#f11946'
+          progress={progress}
+        />
 
-          {/* Top loader */}
-          <LoadingBar
-            height={3}
-            color='#f11946'
-            progress={this.state.progress}
-          />
-
-          {/* Categories */}
-          <Switch>
-            <Route exact path="/"><News setProgress={this.setProgress} apiKey={this.apiKey} key="general" category="general" country="in" pageSize={this.pageSize} /></Route>
-            <Route exact path="/business"><News setProgress={this.setProgress} apiKey={this.apiKey} key="business" category="business" country="in" pageSize={this.pageSize} /></Route>
-            <Route exact path="/entertainment"><News setProgress={this.setProgress} apiKey={this.apiKey} key="entertainment" category="entertainment" country="in" pageSize={this.pageSize} /></Route>
-            <Route exact path="/health"><News setProgress={this.setProgress} apiKey={this.apiKey} key="health" category="health" country="in" pageSize={this.pageSize} /></Route>
-            <Route exact path="/science"><News setProgress={this.setProgress} apiKey={this.apiKey} key="science" category="science" country="in" pageSize={this.pageSize} /></Route>
-            <Route exact path="/sports"><News setProgress={this.setProgress} apiKey={this.apiKey} key="sports" category="sports" country="in" pageSize={this.pageSize} /></Route>
-            <Route exact path="/technology"><News setProgress={this.setProgress} apiKey={this.apiKey} key="technology" category="technology" country="in" pageSize={this.pageSize} /></Route>
-          </Switch>
-        </Router>
-      </div>
-    )
-  }
+        {/* Categories */}
+        <Switch>
+          <Route exact path="/"><News setProgress={setProgress} apiKey={apiKey} key="general" category="general" country="in" pageSize={pageSize} /></Route>
+          <Route exact path="/business"><News setProgress={setProgress} apiKey={apiKey} key="business" category="business" country="in" pageSize={pageSize} /></Route>
+          <Route exact path="/entertainment"><News setProgress={setProgress} apiKey={apiKey} key="entertainment" category="entertainment" country="in" pageSize={pageSize} /></Route>
+          <Route exact path="/health"><News setProgress={setProgress} apiKey={apiKey} key="health" category="health" country="in" pageSize={pageSize} /></Route>
+          <Route exact path="/science"><News setProgress={setProgress} apiKey={apiKey} key="science" category="science" country="in" pageSize={pageSize} /></Route>
+          <Route exact path="/sports"><News setProgress={setProgress} apiKey={apiKey} key="sports" category="sports" country="in" pageSize={pageSize} /></Route>
+          <Route exact path="/technology"><News setProgress={setProgress} apiKey={apiKey} key="technology" category="technology" country="in" pageSize={pageSize} /></Route>
+        </Switch>
+      </Router>
+    </div>
+  )
 }
+
+export default App
--- src/components/News.js
+++ src/components/News.js
@@ -12,6 +12,8 @@ const News = (props) => {
     const [page, setPage] = useState(1)
     const [totalResults, setTotalResults] = useState(0)
 
+    
+
     const capitalizeFirstLetter = (string) => {
         return string.charAt(0).toUpperCase() + string.slice(1)
     }
@@ -31,12 +33,15 @@ const News = (props) => {
     }
 
     useEffect(() => {
+        document.title = `${capitalizeFirstLetter(props.category)} - NewsMonkey`;
         updateNews();
-    })
+        // eslint-disable-next-line
+    }, [])
 
     const fetchMoreData = async () => {
+        
+        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`;
         setPage(page + 1)
-        const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json()
         setaArticles(articles.concat(parsedData.articles));
@@ -45,7 +50,7 @@ const News = (props) => {
 
     return (
         <>
-            <h1 className="text-center" style={{ margin: '30px 0px' }}>NewsMonkey - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
+            <h1 className="text-center" style={{ margin: '30px 0px', marginTop: '90px' }}>NewsMonkey - Top {capitalizeFirstLetter(props.category)} Headlines</h1>
 
             {loading && <Spinner />}
 
