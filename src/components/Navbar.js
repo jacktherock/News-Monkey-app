cd211dea4ebb9c104c63ddf7e4d2482bbbe49326
Abhijeet <sonawaneabhijeet273@gmail.com>
2021-10-10 13:46:41 +0530

    Updated Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
 import React from 'react'
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+
+    let location = useLocation();
 
     return (
         <div>
@@ -12,14 +14,39 @@ const Navbar = () => {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                                {/* <Link className="nav-link active" aria-current="page" to="/">Home</Link> */}
+                                <Link className={`nav-link ${location.pathname === "/" ? "active" : ""}`} aria-current="page" to="/">Home</Link>
                             </li>
-                            <li className="nav-item"><Link className="nav-link" to="/business"> Business</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/entertainment"> Entertainment</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/health"> Health</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/science"> Science</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/sports"> Sports</Link></li>
-                            <li className="nav-item"><Link className="nav-link" to="/technology"> Technology</Link></li>
+
+                            <li className="nav-item">
+                                {/* <Link className="nav-link" to="/business"> Business</Link> */}
+                                <Link className={`nav-link ${location.pathname === "/business" ? "active" : ""}`} aria-current="page" to="/business">Business</Link>
+                            </li>
+
+                            <li className="nav-item">
+                                {/* <Link className="nav-link" to="/entertainment"> Entertainment</Link> */}
+                                <Link className={`nav-link ${location.pathname === "/entertainment" ? "active" : ""}`} aria-current="page" to="/entertainment">Entertainment</Link>
+                            </li>
+
+                            <li className="nav-item">
+                                {/* <Link className="nav-link" to="/health"> Health</Link> */}
+                                <Link className={`nav-link ${location.pathname === "/health" ? "active" : ""}`} aria-current="page" to="/health">Health</Link>
+                            </li>
+
+                            <li className="nav-item">
+                                {/* <Link className="nav-link" to="/science"> Science</Link> */}
+                                <Link className={`nav-link ${location.pathname === "/science" ? "active" : ""}`} aria-current="page" to="/science">Science</Link>
+                            </li>
+
+                            <li className="nav-item">
+                                {/* <Link className="nav-link" to="/sports"> Sports</Link> */}
+                                <Link className={`nav-link ${location.pathname === "/sports" ? "active" : ""}`} aria-current="page" to="/sports">Sports</Link>
+                            </li>
+
+                            <li className="nav-item">
+                                {/* <Link className="nav-link" to="/technology"> Technology</Link> */}
+                                <Link className={`nav-link ${location.pathname === "/technology" ? "active" : ""}`} aria-current="page" to="/technology">Technology</Link>
+                            </li>
                         </ul>
                     </div>
                 </div>
